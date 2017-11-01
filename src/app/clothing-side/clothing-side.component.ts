835d2e3ea2ac5df86d35d097f7b3cbc17002b2c7
Romain Marecat <romain.marecat@gmail.com>
2017-11-01 22:43:19 +0100

    Fix aot build

--- src/app/clothing-side/clothing-side.component.ts
+++ src/app/clothing-side/clothing-side.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IClothing } from './../shared/clothing/i-clothing';
 
 @Component({
   selector: 'app-clothing-side',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clothing-side.component.scss']
 })
 export class ClothingSideComponent implements OnInit {
+  @Input() clothing: IClothing;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
