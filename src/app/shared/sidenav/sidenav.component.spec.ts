e88f9505fcfc3089764eaca3cef4bab4527681ed
Romain Marecat <romain.marecat@gmail.com>
2017-11-03 11:33:14 +0100

    Commencement sur les test

--- src/app/shared/sidenav/sidenav.component.spec.ts
+++ src/app/shared/sidenav/sidenav.component.spec.ts
@@ -1,5 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  MatCardModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatButtonModule,
+  MatGridListModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+} from '@angular/material';
+import { LoaderComponent } from './../loader/loader.component';
 import { SidenavComponent } from './sidenav.component';
 
 describe('SidenavComponent', () => {
@@ -8,7 +22,13 @@ describe('SidenavComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SidenavComponent ]
+      imports: [
+         MatGridListModule,
+         MatSidenavModule,
+         MatCardModule,
+         MatToolbarModule,
+      ],
+      declarations: [ SidenavComponent, LoaderComponent ]
     })
     .compileComponents();
   }));
--- src/app/shared/user/user.service.spec.ts
+++ src/app/shared/user/user.service.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { environment } from './../../../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 describe('UserService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+      AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireAuthModule,],
       providers: [UserService]
     });
   });
