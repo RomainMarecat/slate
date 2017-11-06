7fa18d383c62c1c244bb4d54bfe14242e7fdcf8f
Romain Marecat <romain.marecat@gmail.com>
2017-11-06 08:43:10 +0100

    Ajout de firebase deployer

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-ng build -aot --env=prod -prod --base-href "http://monpullmoche.com/"
+ng build -aot --env=prod -prod --base-href "https://monpullmoche.com/"
 cp dist/index.html dist/404.html
 cp src/sitemap.xml dist/sitemap.xml
 cp src/robots.txt dist/robots.txt
 cp src/CNAME dist/CNAME
 npm run precache
-ngh
+firebase login --reauth
+firebase deploy
 exit 0
