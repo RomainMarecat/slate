e59e55445a116335562cd67183835e8ad97ac6ea
Romain Marecat <romain.marecat@gmail.com>
2018-03-07 09:26:50 +0100

    [ci skip] added cards

--- src/shared/material/layout/card/card.component.ts
+++ src/shared/material/layout/card/card.component.ts
@@ -3,11 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: [ './card.component.scss' ]
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  now = new Date();
+  colors: string[] = [
+    'Red',
+    'Blue',
+    'White',
+    'Black',
+    'Yellow',
+    'Green'
+  ];
+
+  seasons: string[] = [
+    'Winter',
+    'Spring',
+    'Summer',
+    'Autumn',
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
