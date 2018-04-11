783595bad723e43b43362f0cb6049a5ad62710dd
RomainMct <33486492+RomainStudent@users.noreply.github.com>
2018-04-11 21:16:56 +0000

    Feature/product car (#149)
    * Added new projects to build :)
    
    * Added negotiable price
    
    * Added agm on car project

--- src/app-menincar/product-edit/car-product-edit.module.ts
+++ src/app-menincar/product-edit/car-product-edit.module.ts
@@ -5,12 +5,17 @@ import { ProductEditRoutingModule } from './product-edit-routing.module';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { SharedModule } from '../../shared/shared.module';
 import { NgArrayPipesModule, RangePipe } from 'ngx-pipes';
+import { AgmCoreModule } from '@agm/core';
+import { environment } from '../../environments/environment.menincar';
 
 @NgModule({
   imports: [
     CommonModule,
     NgArrayPipesModule,
     SharedModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.googleMapApiKey
+    }),
     ProductEditRoutingModule
   ],
   declarations: [ProductEditComponent],
