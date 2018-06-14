1fea5b1d1fea0a8ce20b8de5104975abf616d96b
Romain Marecat <romain.marecat@gmail.com>
2018-06-14 08:44:50 +0200

    Added nice dashboard features view

--- src/app-alr/dashboard/dashboard/dashboard.component.ts
+++ src/app-alr/dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from 'shared/menu/menu.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: [ './dashboard.component.scss' ]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private menuService: MenuService) {
+  }
 
   ngOnInit() {
+    this.menuService.nextTitle('');
   }
 
 }
