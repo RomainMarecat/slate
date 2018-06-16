f0b2874ac1d62f2a63ac50a1cd51f495b63c5132
Romain Marecat <romain.marecat@gmail.com>
2018-06-16 12:28:23 +0200

    Fixed material color theme

--- src/app-alr/chart/chart-dynamic/chart-dynamic.component.ts
+++ src/app-alr/chart/chart-dynamic/chart-dynamic.component.ts
@@ -3,21 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-chart-dynamic',
   templateUrl: './chart-dynamic.component.html',
-  styleUrls: [ './chart-dynamic.component.scss' ]
+  styleUrls: ['./chart-dynamic.component.scss']
 })
 export class ChartDynamicComponent implements OnInit {
-// lineChart
-  public lineChartData: Array<any> = [
-    [ 65, 59, 80, 81, 56, 55, 40 ],
-    [ 28, 48, 40, 19, 86, 27, 90 ]
+  // lineChart
+  lineChartData: Array<any> = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
   ];
-  public lineChartLabels: Array<any> = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July' ];
-  public lineChartType: string = 'line';
-  public pieChartType: string = 'pie';
+  lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  lineChartType = 'line';
+  pieChartType = 'pie';
 
   // Pie
-  public pieChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail Sales' ];
-  public pieChartData: number[] = [ 300, 500, 100 ];
+  pieChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+  pieChartData: number[] = [300, 500, 100];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 
   public randomizeType(): void {
     this.lineChartType = this.lineChartType === 'line' ? 'bar' : 'line';
@@ -31,11 +37,4 @@ export class ChartDynamicComponent implements OnInit {
   public chartHovered(e: any): void {
     console.log(e);
   }
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
-
 }
