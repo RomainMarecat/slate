c3b6f5c22577c057d1ad355cfcc2266deb081a91
Romain Marecat <romain.marecat@gmail.com>
2018-09-06 08:43:20 +0200

    Reset de la position quand on change de route

--- src/app-alr/core/root.component.ts
+++ src/app-alr/core/root.component.ts
@@ -7,11 +7,13 @@ import { UserService } from '../../shared/user/shared/user.service';
 import { LoaderService } from '../../shared/loader/loader.service';
 import { I18nService } from '../../shared/i18n/i18n.service';
 import { Favicon } from '../../shared/favicon/favicon.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
-  styleUrls: [ './root.component.scss' ]
+  styleUrls: ['./root.component.scss']
 })
 export class AppRootComponent implements OnInit, OnDestroy {
 
@@ -32,8 +34,25 @@ export class AppRootComponent implements OnInit, OnDestroy {
               private ccService: NgcCookieConsentService,
               private meta: Meta,
               private title: Title,
-              private favicon: Favicon) {
+              private favicon: Favicon,
+              private router: Router) {
+    // previous url
+    let previousRoute = router.routerState.snapshot.url;
+
+    // Route subscriber
+    router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((data: NavigationEnd) => {
+        // We want to reset the scroll position on navigation except when navigating within
+        // the page for a single component.
+        if (!isNavigationWithinComponentView(previousRoute, data.urlAfterRedirects)) {
+          resetScrollPosition();
+        }
+
+        previousRoute = data.urlAfterRedirects;
+      });
   }
+
 
   ngOnInit() {
     // subscribe to cookieconsent observables to react to main events
@@ -72,3 +91,30 @@ export class AppRootComponent implements OnInit, OnDestroy {
     this.revokeChoiceSubscription.unsubscribe();
   }
 }
+
+/**
+ * If component is new component with a view
+ * @param {string} oldUrl
+ * @param {string} newUrl
+ * @returns {boolean}
+ */
+function isNavigationWithinComponentView(oldUrl: string, newUrl: string) {
+  const componentViewExpression = /components\/(\w+)/;
+  return oldUrl && newUrl
+    && componentViewExpression.test(oldUrl)
+    && componentViewExpression.test(newUrl)
+    && oldUrl.match(componentViewExpression)[1] === newUrl.match(componentViewExpression)[1];
+}
+
+/**
+ * Reset scroll top 0 if side nav mat content exists
+ */
+function resetScrollPosition() {
+  if (typeof document === 'object' && document) {
+    const sidenavContent = document.querySelector('.mat-drawer-content');
+    if (sidenavContent) {
+      sidenavContent.scrollTop = 0;
+    }
+  }
+}
+
