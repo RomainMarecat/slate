3416966decb8fd4216e52c16cd686927ee6c03e7
Romain Marecat <romain.marecat@gmail.com>
2019-04-25 22:29:42 +0200

    Fix env stripe key

--- src/app-store/environments/environment.ts
+++ src/app-store/environments/environment.ts
@@ -28,5 +28,6 @@ export const environment = {
   clientAdSense: 'ca-pub-4195199088767183',
   slotAdSense: 123456,
   slackToken: 'https://hooks.slack.com/services/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',
-  facebook_app_id: '813234845524646'
+  facebook_app_id: '813234845524646',
+  stripeKey: 'pk_test_ZMBhVWlsAzGDErk8PFH28TWX'
 };
