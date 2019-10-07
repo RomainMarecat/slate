69fa22faf883d0c63d1792993cc61758182b912b
Romain Marecat <romain.marecat@gmail.com>
2019-10-07 23:11:55 +0200

    Fix map service mock

--- src/shared/map/shared/mock-map.service.ts
+++ src/shared/map/shared/mock-map.service.ts
@@ -1,9 +1,27 @@
-import { Observable, of } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+import { Map as LeafletMap } from 'leaflet';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { skipWhile } from 'rxjs/operators';
 import { Map } from './map';
 import { mockMaps } from './mock-map';
 
 export class MockMapService {
+
+  map$: BehaviorSubject<LeafletMap> = new BehaviorSubject<LeafletMap>(null);
+
   getMaps(): Observable<Map[]> {
     return of(mockMaps);
   }
+
+  onMapReady(map: LeafletMap) {
+    this.map$.next(null);
+  }
+
+  getLeafletMap(): Observable<LeafletMap> {
+    return this.map$.asObservable()
+      .pipe(
+        skipWhile((map) => !map)
+      );
+  }
+
 }
